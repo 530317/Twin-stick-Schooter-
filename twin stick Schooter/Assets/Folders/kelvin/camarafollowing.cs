a7e39473bbb0c855c2d983b1c117235f96cf9713
kelvin de Jong <adelaar141@gmail.com>
2019-05-07 12:25:56 +0200

    update

---
+++ twin stick Schooter/Assets/Folders/kelvin/camarafollowing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camarafollowing : MonoBehaviour
+{
+    public Transform target;
+
+    //public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        Vector3 smoothedposition = Vector3.Lerp(transform.position, desiredposition,0f);
+        transform.position = smoothedposition;
+
+        transform.LookAt(target);
+
+    }
+}
---
+++ twin stick Schooter/Assets/Folders/kelvin/schooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class schooting : MonoBehaviour
+{
+    private float bulletSpeed = 1100;
+    public GameObject bullet;
+
+    // Use this for initialization
+    private void fire()
+    {
+        // schoot
+        GameObject tempbullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        Rigidbody temprigidbodybullet = tempbullet.GetComponent<Rigidbody>();
+        temprigidbodybullet.AddForce(temprigidbodybullet.transform.forward * bulletSpeed);
+        Destroy(tempbullet, 0.5f);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fire();
+        }
+    }
+}
