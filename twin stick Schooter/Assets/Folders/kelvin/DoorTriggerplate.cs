59def1d31268aec248096dc61928af6f000ef96d
kelvin de Jong <adelaar141@gmail.com>
2019-05-10 14:43:38 +0200

    door

---
+++ twin stick Schooter/Assets/Folders/kelvin/DoorTriggerplate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerplate : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    private void OnTriggerEnter(Collider other)
+    {
+        door.transform.position += new Vector3(0, 3, 0);
+    }
+}
