9c35ca4d5d52acf5cb1ae8888ea924ce218dfaf8
Quinten Reijns <529357@student.glu.nl>
2019-06-04 09:36:48 +0200

    TEST

---
+++ twin stick Schooter/Assets/Folders/quinten/Patricles System/ProjectileMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+
+    public float speed;
+    public float fireRate;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        StartCoroutine(schooting());
+
+    }
+
+    IEnumerator schooting()
+    {        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        yield return new WaitForSeconds(2f);
+       
+    }
+}
