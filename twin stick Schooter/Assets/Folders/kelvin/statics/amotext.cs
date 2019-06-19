dbb8a2c715c1bc21416d27e6beaa666d85b334ce
kelvin de Jong <adelaar141@gmail.com>
2019-06-19 15:16:13 +0200

    ui 2

--- twin stick Schooter/Assets/Folders/kelvin/statics/amotext.cs
+++ twin stick Schooter/Assets/Folders/kelvin/statics/amotext.cs
@@ -8,15 +8,21 @@ public class Amotext : MonoBehaviour
     public Text text;
     private int currentamo;
     private int maxamo;
+
+    public Image amobar;
+    private float omrekenen;
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        omrekenen = currentamo;
+        omrekenen = omrekenen / 5;
+        amobar.fillAmount = omrekenen;
         text.text = "amo:" + currentamo + "/" + maxamo;
         currentamo = Guncontroller.currentAmmo;
         maxamo = Guncontroller.maxamo;
